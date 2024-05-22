570d8f84288c781299913b50c7e17ab52eeb82b5
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-05-22 16:26:56 -0300

    Retirando comentarios

--- Python Orientação a Objetos/1 - Classe/definicao_classe.py
+++ Python Orientação a Objetos/1 - Classe/definicao_classe.py
@@ -1,28 +1,13 @@
-# As classes definem atributos e comportamentos que caracterizam os objetos,
-# enquanto os objetos são instâncias das classes.
-
-# O nome da classe, segue convenção CamelCase
 class Bicicleta:
-    # Variavel da classe, todos teriam o mesmo numero de rodas
     numero_de_rodas = 2
 
-    # Construtor ou Método Construtor
     def __init__(self, cor, modelo, ano, valor, aro=18):
-        # self- refere ao objeto atual (explicito),porém poderia ser outro nome
-        # self, usado para acessar variaveis que pertencem a classe
-        # atributos - inicializar os atributos
-        # atributos, são como variaveis que pertencem a instancia de uma classe
-        # Variaveis de instância
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
         self.aro = aro
 
-    # método
-    # não confundir com função, e nem esquecer do self
-    # Podem acessar e modificar atributos, até executar alguma ação
-    # Definem comportamentos para os objetos da classe
     def buzinar(self):
         print('Plimmmmmmmmm')
 
@@ -38,26 +23,15 @@ class Bicicleta:
 
     def __str__(self):
         return f"{self.__class__.__name__}:{', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
-    '''
-    def __str__(self):
-        return f'{self.__class__.__name__}: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}'
-    '''
 
 
-# Criando objeto
-# Chama pelo nome da classe, como se fosse uma função
 b1 = Bicicleta('vermelho', 'caloi', 2022, 600)
-
-# Posso acessar um método via ponto
-b1.buzinar()  # Bicicleta.buzinar(b1)
+b1.buzinar()
 b1.correr()
 b1.parar()
 print(b1.get_cor())
-
-b1.cor = "azul"  # Alterar um valor
-
-# Posso acessar também um atributo com o ponto
+b1.cor = "azul"
 print(b1.cor)
-del b1.cor  # deletar
+del b1.cor
 print(b1.numero_de_rodas)
 print(b1)
