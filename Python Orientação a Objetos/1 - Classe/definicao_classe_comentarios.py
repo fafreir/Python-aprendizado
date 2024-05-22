1ca8c994e6cca6af3c3fb7f957264252817854a8
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-05-22 16:51:27 -0300

    Modificando comentários

--- Python Orientação a Objetos/1 - Classe/definicao_classe_comentarios.py
+++ Python Orientação a Objetos/1 - Classe/definicao_classe_comentarios.py
@@ -1,28 +1,41 @@
-# As classes definem atributos e comportamentos que caracterizam os objetos,
-# enquanto os objetos são instâncias das classes.
-
+'''
+As classes definem atributos e comportamentos que caracterizam os objetos,
+enquanto os objetos são instâncias das classes.
+'''
 # O nome da classe, segue convenção CamelCase
+
+
 class Bicicleta:
     # Variavel da classe, todos teriam o mesmo numero de rodas
     numero_de_rodas = 2
 
     # Construtor ou Método Construtor
     def __init__(self, cor, modelo, ano, valor, aro=18):
-        # self- refere ao objeto atual (explicito),porém poderia ser outro nome
-        # self, usado para acessar variaveis que pertencem a classe
-        # atributos - inicializar os atributos
-        # atributos, são como variaveis que pertencem a instancia de uma classe
+        '''
+        # self
+        - refere ao objeto atual (explicito)
+        - poderia ser utilizado outro nome, por convenção utiliza o self
+        - usado para acessar variaveis/atributos que pertencem a classe
+        - inicializa os atributos
+
+        # Atributo
+        - são como variaveis que pertencem a instancia de uma classe
+
         # Variaveis de instância
+        '''
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
         self.aro = aro
 
-    # método
-    # não confundir com função, e nem esquecer do self
-    # Podem acessar e modificar atributos, até executar alguma ação
-    # Definem comportamentos para os objetos da classe
+    '''
+    # método 
+    - não confundir com função, e nem esquecer do self
+    - Podem acessar e modificar atributos, até executar alguma ação
+    - Definem comportamentos para os objetos da classe
+    '''
+
     def buzinar(self):
         print('Plimmmmmmmmm')
 
@@ -38,14 +51,17 @@ class Bicicleta:
 
     def __str__(self):
         return f"{self.__class__.__name__}:{', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
     '''
     def __str__(self):
         return f'{self.__class__.__name__}: cor={self.cor}, modelo={self.modelo}, ano={self.ano}, valor={self.valor}'
     '''
 
 
-# Criando objeto
-# Chama pelo nome da classe, como se fosse uma função
+'''
+ Criando objeto
+ Chama pelo nome da classe, como se fosse uma função
+'''
 b1 = Bicicleta('vermelho', 'caloi', 2022, 600)
 
 # Posso acessar um método via ponto
