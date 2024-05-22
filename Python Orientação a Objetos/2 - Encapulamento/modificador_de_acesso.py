caf591a3920f37e2ff64ab76b94ac47bc26ab706
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-05-22 16:05:11 -0300

    Primeiros arquivos referente ao aprendizado do Python para Orientação a Objetos

---
+++ Python Orientação a Objetos/2 - Encapulamento/modificador_de_acesso.py
@@ -0,0 +1,23 @@
+# Modificador de acesso: define o escopo
+# Existem 3 modificadores: public, private (__) e protected(_)
+# Em Python, utilizam uma convenção de não acessar diretamente private e protected
+
+class Conta:
+    def __init__(self, nro_agencia, saldo=0):
+        self._saldo = saldo
+        self.nro_agencia = nro_agencia
+
+    def depositar(self, valor):
+        self._saldo += valor
+
+    def sacar(self, valor):
+        self._saldo -= valor
+
+    def mostrar_saldo(self):
+        return self._saldo
+
+
+conta = Conta("0001", 100)
+conta.depositar(100)
+print(conta.nro_agencia)
+print(conta.mostrar_saldo())
---
+++ Python Orientação a Objetos/3 - Herança/heranca.py
@@ -0,0 +1,41 @@
+# Na herança é herdado: atributos e métodos de outra classe
+# classe pai ou base - a classe de onde os atributos e métodos são herdados
+# classe filha - é a que herda
+
+class Veiculo:  # classe pai ou base
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print("Ligando o motor")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Motocicleta(Veiculo):  # classe filha
+    pass
+
+
+class Carro(Veiculo):
+    pass
+
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado.")
+
+
+moto = Motocicleta("preta", "abc-1234", 2)
+print(moto)
+moto.ligar_motor()
+
+caminhao = Caminhao("roxo", "gfd-8712", 8, True)
+print(caminhao)
+caminhao.esta_carregado()
