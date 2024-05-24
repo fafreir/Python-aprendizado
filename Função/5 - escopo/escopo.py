6455c2056d901391e5b007f58af6e2ee27bc55e7
Fabrício Adriel <fabricioadriel@yahoo.com.br>
2024-05-24 05:40:26 -0300

    Realizado novas mudanças

--- Função/5 - escopo/escopo.py
+++ Função/5 - escopo/escopo.py
@@ -21,22 +21,38 @@ Cada função tem seu próprio escopo
 """
 
 x = 1
+y = 20
 
 
 def escopo():  # Essa função não consegue acessar valores da função outra_funcao
-    global x  # altero o escopo, com isso a variavel x irá alterar o valor
+    # global x  # altero o escopo, com isso a variavel x irá alterar o valor de 1 para 10
     x = 10
+    print(f'Na função escopo: x = {x}')
 
     def outra_funcao():  # Essa função consegue acessar valores da função escopo
         global x
         x = 11
         y = 2  # somente essa função consegue acessar, devido ser local
-        print(x, y)
+        print(
+            f'Na função outra_funcao: x = {x} e y = {y}')
+
+    def outra_funcao2():
+        x = 12  # Irá pegar apenas o valor x local da propria função e do valor global de y
+        print(
+            f'Na função outra_funcao2: x = {x} e y = {y}')
 
     outra_funcao()
-    print(x)
+    outra_funcao2()
+    # Cuidado: aqui ainda o escopo é local, porém está como global x = 10
+    # Caso a função escopo, x não fosse global, aqui o x não alteraria de valor
+    # Mesmo passando por outras funções abaixo, pois não acessa os outros escopos
+    print(
+        f'Executado dentro da função escopo, depois de passar pelas funções outra_funcao e outra_funcao2: x = {x}')
 
 
-print(x)  # 1
-escopo()  # 11 2 11
-print(x)  # 11
+print(f"Inicialmente o x = {x} e y = {y}")
+escopo()  # Acessando a função escopo
+# Vai pegar os valores globais, já não está sendo executado dentro da função
+# x da função outra_funcao
+print(
+    f"Depois de passar pelas funções escopo, outra_funcao e outra_funcao2: x = {x} e y = {y}")
